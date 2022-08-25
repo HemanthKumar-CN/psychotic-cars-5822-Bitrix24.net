a1a6992388575405bfece8819c7915394a7dca2a
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-08-25 15:39:32 +0530

    working on the right drawer

--- bitrix-frontend/src/App.js
+++ bitrix-frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 function App() {
+<<<<<<< Updated upstream
   return <div>
    
   </div>;
+=======
+  return (
+    <div>
+      <MainPage/>
+    </div>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App;
