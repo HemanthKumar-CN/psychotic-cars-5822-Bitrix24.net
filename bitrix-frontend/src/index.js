fba1b9f544d9e8ab99995c7446835f4bca1902f8
manik1848 <manikprakash1848@gmail.com>
2022-08-26 12:53:34 +0530

    Side Navbar

--- bitrix-frontend/src/index.js
+++ bitrix-frontend/src/index.js
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import {BrowserRouter} from "react-router-dom";
+ import { Provider } from 'react-redux';
+import { store } from "./Redux/store";
 
 // import axios from "axios";
 
