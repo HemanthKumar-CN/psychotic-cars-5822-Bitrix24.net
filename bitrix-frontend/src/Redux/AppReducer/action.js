ce2d2dcdfd6b82bfefc1c15fb08c718e2a9793e8
deshmukh612 <99964682+deshmukh612@users.noreply.github.com>
2022-08-27 11:30:29 +0530

    Add task UI and Post Request

---
+++ bitrix-frontend/src/Redux/AppReducer/action.js
@@ -0,0 +1,20 @@
+import * as types from "./actionTypes";
+import axios from "axios";
+
+
+const addTask = (payload) => (dispatch) => {
+  dispatch({ type: types.ADD_TASK_REQUEST });
+  return axios
+    .post(" http://localhost:8080/tasks", payload)
+    .then((r) => {
+      console.log(r.data)
+      dispatch({ type: types.ADD_TASK_SUCCESS, payload: r.data });
+      return types.ADD_TASK_SUCCESS;
+    })
+    .catch((e) => {
+      dispatch({ type: types.ADD_TASK_FAILURE, payload: e });
+      return types.ADD_TASK_FAILURE;
+    });
+};
+
+export { addTask };
---
+++ bitrix-frontend/src/Redux/AppReducer/reducer.js
@@ -0,0 +1,37 @@
+import * as types from "./actionTypes";
+
+const initialState = {
+  tasks: [],
+  isLoading: false,
+  isError: false,
+};
+
+const reducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case types.ADD_TASK_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+      case types.ADD_TASK_SUCCESS:
+      return {
+        ...state,
+        tasks: [...state.tasks, payload],
+        isLoading: false,
+        isError: false,
+      };
+      case types.ADD_TASK_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };     
+
+    default:
+      return state;
+  }
+};
+
+export { reducer };
