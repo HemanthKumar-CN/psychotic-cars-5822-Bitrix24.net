ed2e3f847e52c4ad0b0d0e2fc24b9c2af988cffa
AnuragPawar-132 <anuragpawar.me@gmail.com>
2022-08-27 12:17:37 +0530

    changes in backend : object having message key in response

--- bitrix-backend/controller/user.js
+++ bitrix-backend/controller/user.js
@@ -10,11 +10,11 @@ userRouter.post("/signup", async (req, res)=>{
     const {username, email, password} = req.body;
     const previous_user = await UserModel.find({email})
     if(previous_user.length !== 0){
-        return res.send("User Already Exist, Please login")
+        return res.send({"message": "User Already Exist, Please login"})
     }
     await bcrypt.hash(password, 4, (err, hash_password)=>{
         if(err){
-            return res.send("Signup failed, Please try again")
+            return res.send({"message": "Signup failed, Please try again"})
         }
         const new_user = new UserModel({
             username : username,
@@ -22,7 +22,7 @@ userRouter.post("/signup", async (req, res)=>{
             password : hash_password
         })
         new_user.save();
-        res.send("Signup Successful")
+        res.send({"message": "Signup Successful"})
     }) 
 })
 
@@ -32,17 +32,17 @@ userRouter.post("/login", async (req, res)=>{
     const hashed_password = search_user.password;
     await bcrypt.compare(password, hashed_password, (err, result)=>{
         if(err){
-            return res.send("Login Failed, please try again")
+            return res.send({"message": "Login Failed, please try again"})
         }
         if(result){
             const token = jwt.sign({email : search_user.email, username : search_user.username, _id: search_user._id}, process.env.SECRET_KEY)
             if(search_user.length === 0){
-                return res.send("Invalid Credential")
+                return res.send({"message": "Invalid Credential"})
             }
-            res.send({"message":"Login Successful", "token": token})
+            res.send({"message":"Login Successful", "token": token, "username":search_user.username })
         }
         else{
-            return res.send("Invalid Credential")
+            return res.send({"message": "Invalid Credential"})
         }
     })
 })
