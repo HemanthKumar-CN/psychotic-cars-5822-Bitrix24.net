ee05fb83676cc893a365e4f5e270d3758ebd41d1
manik1848 <manikprakash1848@gmail.com>
2022-08-28 12:49:14 +0530

    Edit slider

--- bitrix-frontend/src/Components/Tasktable.jsx
+++ bitrix-frontend/src/Components/Tasktable.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
+import styles from "./AddTask/Tags.module.css";
 import {
   Table,
   Thead,
@@ -11,22 +12,69 @@ import {
   Tag,
   useToast,
 } from "@chakra-ui/react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Stack,
+  Box,
+  Input,
+  FormLabel,
+  Textarea,
+  Button,
+  useDisclosure,
+  Checkbox,
+  Flex,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+// import {  } from "react-redux";
 import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
 import { FaUserCircle } from "react-icons/fa";
-import { deleteTask, getTask } from "../Redux/AppReducer/action";
+import { deleteTask, getTask,addTask } from "../Redux/AppReducer/action";
 
 export const Tasktable = () => {
   const tasks = useSelector((state) => state.AppReducer.tasks);
   const toast = useToast();
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [title, setTitle] = useState("");
+  const [highPriority, sethighPriority] = useState(false);
+  const [description, setDescription] = useState("");
+  const [creator, setCreator] = useState("");
+  const [assigned, setAssigned] = useState([]);
+  const [deadline, setDeadline] = useState("");
+  const [project, setProject] = useState("");
+  const [time, setTime] = useState("");
+  const [tag, setTag] = useState("");
   const token = localStorage.getItem("token");
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getTask(token));
   }, []);
-  // console.log(tasks);
 
+  const addTaskHandler=(id)=>{
+    console.log(id);
+  }
+  const handleKeyDown = (e) => {
+    // console.log(e.key)
+    if (e.key !== "Enter") return;
+    console.log(e.target.value);
+    const value = e.target.value;
+    // if(value.trim()) return
+    setAssigned([...assigned, value]);
+    e.target.value = "";
+  };
   const handleDelete = async (id) => {
     // console.log("Delete", id);
     dispatch(deleteTask(id, token)).then((r) => {
@@ -50,6 +98,156 @@ export const Tasktable = () => {
 
   return (
     <div>
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="xl">
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader borderBottomWidth="1px" backgroundColor={"#f4f7f8"}>
+              New task
+            </DrawerHeader>
+            <DrawerBody>
+              <Stack spacing="24px">
+                <Flex justifyContent={"space-between"}>
+                  <Input
+                    id="title"
+                    placeholder="Things to do"
+                    width="60%"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                  <Checkbox
+                    checked={highPriority}
+                    onChange={(e) => {
+                      sethighPriority(e.target.checked);
+                    }}
+                  >
+                    Priority
+                  </Checkbox>
+                </Flex>
+                <Box>
+                  <FormLabel htmlFor="desc">Description</FormLabel>
+                  <Textarea
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </Box>
+                <Box w="60%" alignItems={"left"}>
+                  <Flex justifyContent={"space-between"}>
+                    <FormLabel htmlFor="username">Created by</FormLabel>
+                    <Input
+                      ml="50px"
+                      id="creator"
+                      placeholder="Add"
+                      width="80%"
+                      value={creator}
+                      onChange={(e) => setCreator(e.target.value)}
+                    />
+                  </Flex>
+                </Box>
+                <Box w="80%">
+                  <Flex justifyContent={"space-between"}>
+                    <FormLabel mt="10px">Responsible person</FormLabel>
+                    <div className={styles.tagsbox}>
+                      {assigned.map((tag, index) => (
+                        <div key={index} className={styles.tagsitem}>
+                          <span className={styles.text}>{tag}</span>
+                          <span className={styles.close}>&times;</span>
+                        </div>
+                      ))}
+                      <input
+                        onKeyDown={handleKeyDown}
+                        type="text"
+                        placeholder="+ Add more"
+                        className={styles.tagsinput}
+                      />
+                    </div>
+                  </Flex>
+                </Box>
+                <Box w="60%">
+                  <Flex justifyContent={"space-between"}>
+                    <FormLabel htmlFor="username">Deadline</FormLabel>
+                    <Input
+                      ml={"65px"}
+                      type="date"
+                      id="deadline"
+                      width="80%"
+                      value={deadline}
+                      onChange={(e) => setDeadline(e.target.value)}
+                    />
+                  </Flex>
+                </Box>
+                <Accordion allowToggle>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                        <Box flex="1" textAlign="left" width="50%">
+                          More
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      <Stack spacing={"24px"}>
+                        <Box w="60%" alignItems={"left"}>
+                          <Flex ml="-1.5px" justifyContent={"space-between"}>
+                            <FormLabel htmlFor="username">Project</FormLabel>
+                            <Input
+                              id="project"
+                              placeholder="Add"
+                              width="70%"
+                              value={project}
+                              onChange={(e) => setProject(e.target.value)}
+                            />
+                          </Flex>
+                        </Box>
+                        <Box w="60%" alignItems={"left"}>
+                          <Flex ml="-1.5px" justifyContent={"space-between"}>
+                            <FormLabel htmlFor="time">Time tracking</FormLabel>
+                            <Input
+                              type="time"
+                              id="time"
+                              width="70%"
+                              value={time}
+                              onChange={(e) => setTime(e.target.value)}
+                            />
+                          </Flex>
+                        </Box>
+                        <Box w="60%" alignItems={"left"}>
+                          <Flex ml="-1.5px" justifyContent={"space-between"}>
+                            <FormLabel htmlFor="username">Tag</FormLabel>
+                            <Input
+                              id="tag"
+                              placeholder="Add"
+                              width="70%"
+                              value={tag}
+                              onChange={(e) => setTag(e.target.value)}
+                            />
+                          </Flex>
+                        </Box>
+                      </Stack>
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+              </Stack>
+            </DrawerBody>
+
+            <DrawerFooter borderTopWidth="1px">
+              <Button variant="outline" mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button
+                bgColor={"#bbed21"}
+                borderRadius="3px"
+                onClick={() => {
+                  addTaskHandler();
+                }}
+              >
+                ADD TASK
+              </Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
       <TableContainer>
         <Table variant="simple" colorScheme="blackAlpha">
           <Thead>
@@ -70,12 +268,12 @@ export const Tasktable = () => {
                       <Td>
                         <div style={{ display: "flex", gap: "5px" }}>
                           <IconButton
-                            onClick={() => handleEdit(e._id)}
+                            onClick={onOpen}
                             icon={<EditIcon />}
                             size="xs"
                           />
                           <IconButton
-                            onClick={() => handleDelete(e._id)}
+                             onClick={()=>handleDelete(e._id)}
                             icon={<DeleteIcon />}
                             size="xs"
                           />
